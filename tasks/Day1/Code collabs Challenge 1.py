64ab978f326a9b15949f45138e8144c4770f9a6f
tracykimani <42501559+tracykimani@users.noreply.github.com>
2020-04-21 18:45:57 +0100

    Add files via upload

---
+++ tasks/Day1/Code collabs Challenge 1.py
@@ -0,0 +1,15 @@
+class Converter:
+
+def introduce_age(age):
+   print('What is my age' + age.num)
+print('what is my age',int(num_2))
+
+
+
+
+num_1 = '20'
+num_2 = '15'
+
+num_1= int(num_1)
+num_2 =int(num_2)
+
